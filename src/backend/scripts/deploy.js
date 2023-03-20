cf1bb1cf556578bd3decdecb76bcf52dbc6ec70f
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-20 21:21:43 +0000

    Deployed the Marketplace contract and initialize in deploy.js

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -7,7 +7,12 @@ async function main() {
 	// deploy contracts here:
 	const NFT = await ethers.getContractFactory('NFT');
 	const nft = await NFT.deploy();
+	const Marketplace = await ethers.getContractFactory('Marketplace');
+	const marketplace = await Marketplace.deploy(1);
+
+	console.log(`marketplace address: ${marketplace.address}`);
 	console.log(`NFT CONTRACT address: ${nft.address}`);
+
 	// For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
 	saveFrontendFiles(nft, 'NFT');
 }
