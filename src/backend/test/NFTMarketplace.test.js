1a67d5dd7ade65dbf3e477eaea18f4929563cf6a
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-03-20 21:45:22 +0000

    Created a New Suite of Unit tests for Minting NFTS

--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -4,6 +4,7 @@ const { expect } = require('chai');
 describe('NFTMarketplace', async function () {
 	let deployer, addr1, addr2, nft, marketplace;
 	const feePercent = 10;
+	const URI = 'Sample URI';
 
 	beforeEach(async function () {
 		// create a fresh contract instance before each test
@@ -27,6 +28,25 @@ describe('NFTMarketplace', async function () {
 		it('Should track the marketplace fee', async function () {
 			expect(await marketplace.feeAccount()).to.equal(deployer.address);
 			expect(await marketplace.feePercent()).to.equal(feePercent);
+		});
+	});
+
+	// eslint-disable-next-line jest/valid-describe-callback
+	describe('Minting NFTS', async function () {
+		it('Should track each minted NFT', async function () {
+			// Mint NFT
+			await nft.connect(addr1).mint(URI);
+			expect(await nft.tokenCount()).to.equal(1);
+			expect(await nft.balanceOf(addr1.address)).to.equal(1);
+			expect(await nft.ownerOf(1)).to.equal(addr1.address);
+			expect(await nft.tokenURI(1)).to.equal(URI);
+
+			// Mint another second NFT
+			await nft.connect(addr2).mint(URI);
+			expect(await nft.tokenCount()).to.equal(2);
+			expect(await nft.balanceOf(addr2.address)).to.equal(1);
+			expect(await nft.ownerOf(2)).to.equal(addr2.address);
+			expect(await nft.tokenURI(2)).to.equal(URI);
 		});
 	});
 });
