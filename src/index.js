d6fb7a0bb64481d6e5d3933a6f967270f6543dab
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-04-16 16:41:02 +0100

    add a BrowserRouter component around your App component to provide the basename property

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css'
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './frontend/components/App';
 import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById("root");
-render( <App />, rootElement);
-
+const rootElement = document.getElementById('root');
+render(
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</BrowserRouter>,
+	rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
