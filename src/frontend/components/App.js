9f71e76acb44b5a9a63549ef754ea7e6f7b42091
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-04-16 15:54:17 +0100

    Instantiate New Provider From Metamask

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import { ethers } from 'ethers';
 
 function App() {
+	//Metamask code Login/COnnect
+	const web3Handler = async () => {
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+	};
 	return (
 		<div>
 			<h1>React App</h1>
