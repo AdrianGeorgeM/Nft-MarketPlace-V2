cce0660afa0cd3e61b4086923a09d477cb668005
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-04-17 19:58:25 +0100

    Add route for "My Listed Items" page in React Router.

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import Navigation from './Navbar';
 import Create from './Create.js';
+import MyListedItems from './MyListedItems';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import MarketplaceAbi from '../contractsData/Marketplace.json';
@@ -45,6 +46,10 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Home marketplace={marketplace} nft={nft} />} />
 				<Route path='/create' element={<Create marketplace={marketplace} nft={nft} />} />
+				<Route
+					path='/my-listed-items'
+					element={<MyListedItems marketplace={marketplace} nft={nft} />}
+				/>
 			</Routes>
 		</div>
 	);
